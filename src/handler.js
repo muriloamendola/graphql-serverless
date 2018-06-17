ac5b6edcaaf0f72a163adec62b7164e7d81f1bf0
Murilo Amêndola <muriloamendola@gmail.com>
2018-06-17 12:31:00 -0300

    Adding example of how to avoid cors problems when using js clients

--- src/handler.js
+++ src/handler.js
@@ -2,10 +2,23 @@ const { graphqlLambda, graphiqlLambda } = require('apollo-server-lambda');
 const myGraphQLSchema = require('./graphql/schema');
 
 const graphqlHandler = graphqlLambda({ schema: myGraphQLSchema });
-
+ 
 const graphiqlHandler = graphiqlLambda({ endpointURL: 'http://localhost:3000/graphql' });
 
 module.exports = {
   graphqlHandler,
   graphiqlHandler
 };
+
+
+// Avoid CORS problem when using Angular Apollo Client for example
+/* const graphqlHandler = (event, context, callback) => {
+  const callbackFilter = (error, output) => {
+    output.headers['Access-Control-Allow-Origin'] = '*';
+    callback(error, output);
+  };
+
+  const handler = graphqlLambda({ schema: myGraphQLSchema });
+
+  return handler(event, context, callbackFilter);
+}; */
